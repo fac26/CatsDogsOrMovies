0d9ca2347f08bf82f2a556c57c8351cfaf79c28c
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-10-14 18:56:04 +0100

    add event listener .js

--- js/gal.js
+++ js/gal.js
@@ -1,8 +1,28 @@
 const random = fetch("https://api.postcodes.io/random/postcodes");
+const output = document.querySelector('output');
+const submit = document.querySelector('#submit');
+
+
+addEventListener('click', submit) {
+
 
 
 random
 .then((response) => {return response.json()})
 .then((data) => {
-    document.cr
+   
+        
+      
+        
+        const newContent = data;
+        output.textContent = newContent;
+      
+        // add the text node to the newly created div
+        // newDiv.appendChild(newContent);
+      
+        // add the newly created element and its content into the DOM
+        // const currentDiv = document.getElementById("div1");
+        // document.body.insertBefore(newDiv, currentDiv);
+      
 })
+}
