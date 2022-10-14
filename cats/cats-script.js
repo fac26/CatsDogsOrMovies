0299e92981d7cfeb6b72a56a2f1cc00e837158db
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-14 22:47:02 +0100

    simplified fetch, added constants for search return limits

--- cats/cats-script.js
+++ cats/cats-script.js
@@ -1,10 +1,9 @@
-fetch("https://api.thecatapi.com/v1/images/search", {
-  method: "GET",
-  headers: { "x-api-key": "live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP" },
-})
-.then((response) => {
-	if (!response.ok) throw new Error(resonse.status);
+const url5 = "https://api.thecatapi.com/v1/images/search?limit=5";
+const url10 = "https://api.thecatapi.com/v1/images/search?limit=10";
+const url20 = "https://api.thecatapi.com/v1/images/search?limit=20";
+const key = "live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
+
+fetch("url5", { headers: { "x-api-key": key } }).then((response) => {
+	if (!response.ok) throw new Error(response.status);
 	return response.json();
-})
-
-
+});
