c6413dc9a4ea691a1b8ff1bcb8c6d621f5d7bda6
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-15 15:31:46 +0100

    ver.1 of reusable fetch function

--- cats/cats-script.js
+++ cats/cats-script.js
@@ -1,9 +1,24 @@
-const url5 = "https://api.thecatapi.com/v1/images/search?limit=5";
-const url10 = "https://api.thecatapi.com/v1/images/search?limit=10";
-const url20 = "https://api.thecatapi.com/v1/images/search?limit=20";
-const key = "live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
+const key = "&live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
+const api = "https://api.thecatapi.com/v1/images/search";
 
-fetch("url5", { headers: { "x-api-key": key } }).then((response) => {
+const limits = {
+	get1: api + key, //request single image
+	get5: api + "&limit=5" + key, //request 5 images
+	get10: api + "&limit=10" + key, //request 10 images
+	get20: api + "&limit=20" + key, //request 20 images
+};
+
+//test if limits are working
+const testLimits = () => {
+	for (let i = 0; i < limits.length; i++) {
+		console.log(limits[i]);
+	}
+};
+
+//reusable fetch function
+async function fetch(getLimit) {
+	const response = await fetch(getLimit);
 	if (!response.ok) throw new Error(response.status);
+	console.log(response);
 	return response.json();
-});
+}
