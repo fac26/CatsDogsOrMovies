ba0236de40291421279e7a1d88e2b4f0b09a17d6
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-16 22:59:08 +0100

    response and json cleanup functions, some variation of reusable fetch

--- cats/cats-script.js
+++ cats/cats-script.js
@@ -1,20 +1,21 @@
 const _key = "&live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
 const _api = "https://api.thecatapi.com/v1/images/search";
-const headers = {
-  "content-type": "application/json",
+/* const _headers = {
   "ratelimit-limit": "10000",
   "ratelimit-remaining": "10000",
   "ratelimit-reset": "10000",
-};
+}; */ //not sure how to implement
 
 const imageLimit = {
-	get1: _api + _key, //request single image
-	get5: _api + "&limit=5" + _key, //request 5 images
-	get10: _api + "&limit=10" + _key, //request 10 images
-	get20: _api + "&limit=20" + _key, //request 20 images
+	//request x amount of images
+	get1: _api + _key,
+	get5: _api + "&limit=5" + _key,
+	get10: _api + "&limit=10" + _key,
+	get20: _api + "&limit=20" + _key,
+	specificImage: _api + `${img_id}` + _key, //what?
 };
 
-//test if limits are working
+//test if limit vars are working
 const checkLimitVariables = () => {
 	const value = [];
 	Object.keys(imageLimit).forEach((key) => {
@@ -23,12 +24,40 @@ const checkLimitVariables = () => {
 	console.table(value);
 };
 
-//reusable fetch function
-async function fetch(getLimit) {
-	const response = await fetch(getLimit);
-	if (!response.ok) throw new Error(response.status);
+//clean .then(response => response.json)
+const getResponse = (response) => {
+	if (!response.ok) {
+		throw new Error(response.status);
+	}
 	console.log(response);
 	return response.json();
-}
+};
+
+//clean .then(json => console.log(json))
+const processResponse = (json) => {
+	{
+		for (let i = 0; i < json.length; i++) {
+			console.log(json[i]);
+		}
+	}
+};
+
+//clean .catch(json => console.error(error))
+const catchError = (error) => {
+	{
+		for (let i = 0; i < error.length; i++) {
+			console.error(error[i]);
+		}
+	}
+};
+
+const fetch = async (url) => {
+	await fetch(url);
+}; //not sure if this does anything here
 
 checkLimitVariables();
+
+fetch(imageLimit.get1);
+await getResponse;
+await processResponse;
+await catchError; // ok?
