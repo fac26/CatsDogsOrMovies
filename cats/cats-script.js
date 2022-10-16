8992f3a3420a2ec3be6ddbf8ab3de9f250a869a0
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-16 20:16:58 +0100

    minor refactor, limit object check rework

--- cats/cats-script.js
+++ cats/cats-script.js
@@ -1,18 +1,20 @@
-const key = "&live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
-const api = "https://api.thecatapi.com/v1/images/search";
+const _key = "&live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
+const _api = "https://api.thecatapi.com/v1/images/search";
 
-const limits = {
-	get1: api + key, //request single image
-	get5: api + "&limit=5" + key, //request 5 images
-	get10: api + "&limit=10" + key, //request 10 images
-	get20: api + "&limit=20" + key, //request 20 images
+const imageLimit = {
+	get1: _api + _key, //request single image
+	get5: _api + "&limit=5" + _key, //request 5 images
+	get10: _api + "&limit=10" + _key, //request 10 images
+	get20: _api + "&limit=20" + _key, //request 20 images
 };
 
 //test if limits are working
-const testLimits = () => {
-	for (let i = 0; i < limits.length; i++) {
-		console.log(limits[i]);
-	}
+const checkLimitVariables = () => {
+	const value = [];
+	Object.keys(imageLimit).forEach((key) => {
+		return value.push(key);
+	});
+	console.table(value);
 };
 
 //reusable fetch function
@@ -22,3 +24,5 @@ async function fetch(getLimit) {
 	console.log(response);
 	return response.json();
 }
+
+checkLimitVariables();
