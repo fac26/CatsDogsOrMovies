035bff66542e03d22c5c597d6e5782ba654581e8
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-10-17 13:20:32 +0100

    add random postcode generator function .js

--- js/gal.js
+++ js/gal.js
@@ -1,33 +1,24 @@
 
 // Postcode API base endpoint and specific endpoints for varying methods. E.g. look up postcode, random postcode etc.    
 const baseEndpoint = 'https://api.postcodes.io';
-const randomEndpoint = `${baseEndpoint}/random/postcodes`
+const randomPostcodeEndpoint = `${baseEndpoint}/random/postcodes`
 
 // Selected elements
 const output = document.querySelector('output');
 const submit = document.querySelector('#submit');
 
 
-addEventListener('click', submit) {
+// const fetchRandom = 
 
-
-
-random
-.then((response) => {return response.json()})
-.then((data) => {
-   
-        
-      
-        
-        const newContent = data;
-        output.textContent = newContent;
-      
-        // add the text node to the newly created div
-        // newDiv.appendChild(newContent);
-      
-        // add the newly created element and its content into the DOM
-        // const currentDiv = document.getElementById("div1");
-        // document.body.insertBefore(newDiv, currentDiv);
-      
-})
+function randomPostcodeGenerator () {
+        fetch(randomPostcodeEndpoint)
+.then(response => response.json())
+.then(randomPostcodeObj => console.log(randomPostcodeObj)
+)
 }
+
+randomPostcodeGenerator();
+
+
+
+
