5eeb031e114040042f667ec3e9f20495f1090047
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-19 20:27:53 +0100

    restart with new approach

--- cats/cats-script.js
+++ cats/cats-script.js
@@ -1,77 +1,45 @@
-//declare variables
-
-const _key = "&live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP"; 
+const _key = "?api_key=live_Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
 const _api = "https://api.thecatapi.com/v1/images/search";
 
-const imageLimit = {
+const form = document.querySelector("form");
+
+const url = {
 	get1: _api + _key,
-	get5: `${_api}&limit=5${_key}`,
-	get10: `${_api}&limit=10${_key}`,
-	get20: `${_api}&limit=20${_key}`,
+	get5: `${_api + _key}&limit=5`,
+	get10: `${_api + _key}&limit=10`,
+	get20: `${_api + _key}&limit=20`,
 	//specificImage: _api + `${img_id}` + _key, //what?
 };
 
-const rateHeaders = {
-	rateLimit: "ratelimit-limit",
-	rateLimitRemaining: "ratelimit-remaining",
-	rateLimitConsumed: "ratelimit-consumed",
-	rateLimitReset: "ratelimit-reset",
-};
-
-//clean up some functionality
 const getResponse = (response) => {
-	if (!response.ok) {
-		throw new Error(response.status);
-	}
-	console.log(response);
-	return response.json();
-};
+  if (!response.ok) { throw new Error(response.status) };
+  return response.json();
+}
 
-const processResponse = (json) => {
-	for (let i = 0; i < json.length; i++) {
-		console.log(json[i]);
-	}
-};
+const processResponse = (data) => {
+  let imageData = data;
+  imageData.map(function(imageData) {
 
-const catchError = (error) => {
-	for (let i = 0; i < error.length; i++) {
-		console.error(error[i]);
-	}
-};
+    let image = document.createElement("img");
+    image.src = `${imageData.url}`;
 
-/* 
-const fetcher = async (url) => {
-  return await fetch((url = getResponse(url)));
-}; not sure if this does anything here
-*/
+    let gridCell = document.createElement('div');
+      //gridCell.classlist.add grid 
+    gridCell.appendChild(image);
 
-//search function use img_id
-//filter function
-//sort function
-//by breed, category, name, alphebetical
+    document.querySelector('#grid').appendChild(gridCell);
+  });
+}
 
-//test if everything reads
-const myDebug = async () => {
-	//don't forget awaits
-	let titleCSS = ["color: orange", "background-color: #444", "padding: 2px 4px", "border-radius: 2px"].join(";");
-	console.log("%cmyDebug:", titleCSS); //display title for debugging
-	console.table(imageLimit); //check searchlimit object
-	//query header limits
-	//console log response status
-	//console log response object
-	//console log any errors
-};
+const catchErrors = (error) => {
+  console.log(error);
+}
 
-//wrap in event listener to debug on keypress
-myDebug();
 
-//wrap fetch requests in event listeners
-
-/*
-fetch(url, {headers}) {
-.then(getResponse)
-.then(processResponse)
-.then(catchError)
-*/
-
-//repeat per limit request
+//wrap each fetch in event listener tied to button for (somefunction)
+fetch(url.get20)
+  .then(getResponse)
+  .then(processResponse, () => {
+    //does this work to further process the response in addition?
+  })
+  .catch(catchErrors);
