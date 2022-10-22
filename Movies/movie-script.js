bd8f196a6b30dea011b2d651799da214e2428540
Niete <nieteis@gmail.com>
2022-10-22 23:18:21 +0100

    Add next & prev page functionality

--- Movies/movie-script.js
+++ Movies/movie-script.js
@@ -10,6 +10,16 @@ const form = document.getElementById("form");
 const search = document.getElementById("search");
 const tagsEl = document.getElementById("tags");
 
+const prev = document.getElementById("previous");
+const next = document.getElementById("next");
+const current = document.getElementById("current");
+
+let currentPage = 1;
+let nextPage = 2;
+let previousPage = 3;
+let lastUrl = "";
+let totalPages = 100;
+
 const genres = [
   {
     id: 28,
@@ -155,12 +165,31 @@ function clearBtn() {
 getMovies(API_URL);
 
 function getMovies(url) {
+  lastUrl = url;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       console.log(data.results);
       if (data.results.length !== 0) {
         showMovies(data.results);
+        currentPage = data.page;
+        nextPage = currentPage + 1;
+        prevPage = currentPage - 1;
+        totalPages = data.total_pages;
+
+        current.innerText = currentPage;
+        if (currentPage <= 1) {
+          prev.classList.add("disabled");
+          next.classList.remove("disabled");
+        } else if (currentPage >= totalPages) {
+          prev.classList.remove("disabled");
+          next.classList.add("disabled");
+        } else {
+          prev.classList.remove("disabled");
+          next.classList.remove("disabled");
+        }
+
+        form.scrollIntoView({ behavior: "smooth" });
       } else {
         main.innerHTML = `<h1 class="no-results">No results found<h1>`;
       }
@@ -217,3 +246,32 @@ form.addEventListener("submit", (e) => {
     getMovies(API_URL);
   }
 });
+
+prev.addEventListener("click", () => {
+  if (previousPage > 0) {
+    pageCall(prevPage);
+  }
+});
+
+next.addEventListener("click", () => {
+  if (nextPage <= totalPages) {
+    pageCall(nextPage);
+  }
+});
+
+function pageCall(page) {
+  let urlSplit = lastUrl.split("?");
+  let queryParams = urlSplit[1].split("&");
+  let key = queryParams[queryParams.length - 1].split("=");
+  if (key[0] != "page") {
+    let url = lastUrl + "&page=" + page;
+    getMovies(url);
+  } else {
+    key[1] = page.toString();
+    let a = key.join("=");
+    queryParams[queryParams.length - 1] = a;
+    let b = queryParams.join("&");
+    let url = urlSplit[0] + "?" + b;
+    getMovies(url);
+  }
+}
