1887f30ce650e409f4fda2fe6b58c25cffaa2c9a
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-10-25 14:06:30 +0100

    bug fixes, sort by breed selection

--- cats/cats-helper-script.js
+++ cats/cats-helper-script.js
@@ -1,24 +1,7 @@
-const KEY = "Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
 const API = "https://api.thecatapi.com/v1/images/search?api_key=live_";
-const	imageURL = API + KEY + "&limit=30";
-
+const KEY = "Q6eMoEl8dw8aPgrxtXNaR2882nOcbaIfr3DCtWipUv5TWQx5wt7we7ui8YpRpYoP";
 const breedSelector = document.querySelector("#breed--selector");
 const grid = document.querySelector("#grid");
-const form = document.querySelector("form");
-
-async function getBreeds() {
-  const response = await fetch("https://api.thecatapi.com/v1/breeds");
-  data = await response.json();
-
-  data.map((breed) => {
-    const option = document.createElement("option");
-
-    option.value = breed.id;
-    option.textContent = breed.name;
-
-    breedSelector.appendChild(option);
-  });
-}
 
 function getResponse(response) {
   if (!response.ok) {
@@ -28,11 +11,16 @@ function getResponse(response) {
   return response.json();
 }
 
-function processResponse(data) {
-  grid.classList.add("grid");
+function getBreeds(data) {
+  data.map((breed) => {
+    const option = document.createElement("option");
+    option.value = breed.id;
+    option.textContent = breed.name;
+    breedSelector.appendChild(option);
+  });
+}
 
-  //filter images by breed
-
+function getImages(data) {
   data.map((image) => {
     const gridCell = document.createElement("div");
     const img = document.createElement("img");
@@ -47,3 +35,4 @@ function processResponse(data) {
 }
 
 const catchErrors = async (error) => { console.log(await error); }
+const clearGrid = () => { grid.innerHTML = ""; }
