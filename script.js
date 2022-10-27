eb95b60e5a10fff5862f717138fa1b427af82774
Niete <nieteis@gmail.com>
2022-10-27 16:40:06 +0100

    Added media queries for front page

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelector(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
